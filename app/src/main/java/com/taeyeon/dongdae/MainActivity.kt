4175e14c3371f55bf96c83f27ecf1d88a70770bd
error0918 <jtaeyeon05@gmail.com>
2022-10-11 12:31:34 +0900

    2022/11/11 12:31

--- app/src/main/java/com/taeyeon/dongdae/MainActivity.kt
+++ app/src/main/java/com/taeyeon/dongdae/MainActivity.kt
@@ -1,14 +1,18 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.taeyeon.dongdae
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.taeyeon.dongdae.ui.theme.DongdaeTheme
 
@@ -18,11 +22,50 @@ class MainActivity : ComponentActivity() {
         setContent {
             DongdaeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Scaffold(
+                    topBar = {
+                        CenterAlignedTopAppBar(
+                            title = { Text(text = stringResource(id = R.string.app_name)) }
+                        )
+                    },
+                    bottomBar = {
+                        NavigationBar() {
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = { /*TODO*/ },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.PlayArrow,
+                                        contentDescription = null
+                                    )
+                                },
+                                label = {
+                                    Text(text = "afd")
+                                }
+                            )
+                            NavigationBarItem(
+                                selected = true,
+                                onClick = { /*TODO*/ },
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.Default.PlayArrow,
+                                        contentDescription = null
+                                    )
+                                },
+                                label = {
+                                    Text(text = "afd")
+                                }
+                            )
+                        }
+                    }
                 ) {
-                    Greeting("Android")
+                    Surface(
+                        modifier = Modifier.fillMaxSize().padding(it),
+                        color = MaterialTheme.colorScheme.background
+                    ) {
+
+                        Greeting("Android")
+                    }
                 }
             }
         }

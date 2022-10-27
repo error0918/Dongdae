fb952704edca34182d2b53e720a04f130369580c
error0918 <jtaeyeon05@gmail.com>
2022-10-27 15:33:29 +0900

    2022/10/27 15:33

--- app/src/main/java/com/taeyeon/dongdae/Community.kt
+++ app/src/main/java/com/taeyeon/dongdae/Community.kt
@@ -7,7 +7,9 @@ package com.taeyeon.dongdae
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
@@ -23,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
@@ -99,7 +102,7 @@ object Community {
                                 .fillMaxSize()
                                 .padding(getCornerSize(shape = MaterialTheme.shapes.medium) + 16.dp)
                         ) {
-                            val (nameText, subNameText, colorView, heartIconButton, heartText, contentImage, contentText, commentColumn) = createRefs()
+                            val (nameText, subNameText, colorView, contentImage, contentText, heart, commentColumn) = createRefs()
                             var nameTextSize by remember { mutableStateOf(IntSize.Zero) }
 
                             Text(
@@ -117,23 +120,54 @@ object Community {
                             Text(
                                 text = "(${"서브네임"})",
                                 modifier = Modifier
-                                    .constrainAs(nameText) {
-                                        centerHorizontallyTo(nameText)
-                                        start.linkTo(nameText.start)
+                                    .constrainAs(subNameText) {
+                                        centerVerticallyTo(nameText)
+                                        start.linkTo(nameText.end, margin = 8.dp)
                                     }
                             )
 
                             Spacer(
                                 modifier = Modifier
+                                    .width(with(LocalDensity.current) { nameTextSize.height.toDp() })
+                                    .height(with(LocalDensity.current) { nameTextSize.height.toDp() })
                                     .background(
                                         color = Color.Cyan,
                                         shape = CircleShape
                                     )
+                                    .border(
+                                        border = BorderStroke(
+                                            width = 1.dp,
+                                            color = LocalContentColor.current
+                                        ),
+                                        shape = CircleShape
+                                    )
                                     .constrainAs(colorView) {
-
+                                        top.linkTo(parent.top)
+                                        end.linkTo(parent.end)
                                     }
                             )
 
+                            Text(
+                                text = "adfafsd".repeat(100),
+                                modifier = Modifier
+                                    .constrainAs(contentText) {
+                                        top.linkTo(nameText.bottom, margin = 8.dp)
+                                    }
+                            )
+
+                            Surface(
+                                onClick = { /*TODO*/ },
+                                shape = CircleShape,
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .constrainAs(heart) {
+                                        top.linkTo(contentText.bottom, margin = 8.dp)
+                                    }
+                            ) {
+                                Row {
+                                    Spacer(modifier = Modifier.width(8.dp))
+                                }
+                            }
 
                         }
                     }

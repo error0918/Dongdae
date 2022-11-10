43c6cc9e410ff1d1557bba6c4b919c188656bb97
error0918 <jtaeyeon05@gmail.com>
2022-11-11 06:44:39 +0900

    2022/11/11 06:43

--- app/src/main/java/com/taeyeon/dongdae/Community.kt
+++ app/src/main/java/com/taeyeon/dongdae/Community.kt
@@ -7,32 +7,20 @@ package com.taeyeon.dongdae
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.*
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
-import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.PeopleOutline
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.compositeOver
-import androidx.compose.ui.graphics.drawscope.clipRect
 import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
@@ -40,10 +28,8 @@ import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Popup
-import androidx.constraintlayout.compose.ConstraintLayout
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.taeyeon.core.Utils
-import com.taeyeon.dongdae.MyView.ChatUnit
 import kotlinx.coroutines.launch
 
 object Community {
@@ -149,7 +135,10 @@ object Community {
                     .weight(1f),
                 state = lazyListState,
                 verticalArrangement = Arrangement.spacedBy(32.dp),
-                contentPadding = PaddingValues(bottom = 32.dp)
+                contentPadding = PaddingValues(
+                    top = 16.dp,
+                    bottom = 32.dp + 16.dp
+                )
             ) {
 
                 item {
@@ -183,7 +172,7 @@ object Community {
                         )
                     )*/
 
-                    A(
+                    MyView.PostUnit(
                         id = id,
                         content = "얘들아 오늘 급식 어떤 거 나와? 🤤",
                         commentList = listOf(
@@ -211,7 +200,7 @@ object Community {
                 }
 
                 item {
-                    A(
+                    MyView.PostUnit(
                         id = id,
                         content = "한국사 수행평가 범위 어디야?",
                         commentList = listOf(
@@ -229,6 +218,39 @@ object Community {
                             )
                         )
                     )
+                }
+
+                item {
+                    ElevatedCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(IntrinsicSize.Min)
+                            .padding(horizontal = 32.dp),
+                        elevation = CardDefaults.elevatedCardElevation(
+                            defaultElevation = 10.dp,
+                            pressedElevation = 10.dp,
+                            focusedElevation = 10.dp,
+                            hoveredElevation = 10.dp,
+                            draggedElevation = 10.dp,
+                            disabledElevation = 10.dp,
+                        ),
+                        colors = CardDefaults.cardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp),
+                            contentColor =  MaterialTheme.colorScheme.onSurface
+                        )
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(getCornerSize(shape = MaterialTheme.shapes.medium) + 16.dp)
+                        ) {
+                            Text(
+                                text = "개시물을 모두 다 보셨습니다. 😄",
+                                style = MaterialTheme.typography.bodyMedium,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
                 }
 
             }
@@ -289,289 +311,6 @@ object Community {
                     text = "글쓰기",
                     modifier = Modifier.padding(start = 8.dp)
                 )
-            }
-        }
-    }
-
-
-    @Composable
-    fun A(
-        id: String,
-        image: ImageBitmap? = null,
-        isSelectable: Boolean = true,
-        content: String,
-        isHeartAble: Boolean = true,
-        commentList: List<MyView.ChatData> = listOf()
-    ) {
-        var isHeart by rememberSaveable { mutableStateOf(false) }
-        var heartCount by rememberSaveable { mutableStateOf(12) }
-        val onHeartClicked = { checked: Boolean ->
-            heartCount += if (checked) 1 else -1
-            isHeart = checked
-        }
-
-        var isCommentShowing by rememberSaveable { mutableStateOf(false) }
-        var isCommenting by rememberSaveable { mutableStateOf(false) }
-        var isDropDownMenuExpanded by remember { mutableStateOf(false) }
-
-        ElevatedCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(IntrinsicSize.Min)
-                .padding(horizontal = 32.dp),
-            elevation = CardDefaults.elevatedCardElevation(
-                defaultElevation = 10.dp,
-                pressedElevation = 10.dp,
-                focusedElevation = 10.dp,
-                hoveredElevation = 10.dp,
-                draggedElevation = 10.dp,
-                disabledElevation = 10.dp,
-            ),
-            colors = CardDefaults.cardColors(
-                containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f).compositeOver(background = MaterialTheme.colorScheme.surface),
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            )
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ) {
-
-                Surface(
-                    shape = MaterialTheme.shapes.medium,
-                    color = MaterialTheme.colorScheme.surfaceColorAtElevation(3.dp),
-                    contentColor = MaterialTheme.colorScheme.onSurface
-                ) {
-
-                    ConstraintLayout(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(getCornerSize(shape = MaterialTheme.shapes.medium) + 16.dp)
-                    ) {
-                        val (nameUnit, contentImage, contentText, heart, dropDownMenuButton) = createRefs()
-
-                        MyView.NameUnit(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .constrainAs(nameUnit) {
-                                    top.linkTo(parent.top)
-                                },
-                            id = id
-                        )
-
-                        image?.let {
-                            Surface(
-                                modifier = Modifier
-                                    .constrainAs(contentImage) {
-                                        top.linkTo(nameUnit.bottom, margin = 8.dp)
-                                    }
-                            ) {
-                                Image(
-                                    bitmap = image,
-                                    contentDescription = null
-                                )
-                            }
-                        }
-
-                        if (isSelectable) {
-                            SelectionContainer(
-                                modifier = Modifier
-                                    .constrainAs(contentText) {
-                                        top.linkTo(
-                                            if (image == null) nameUnit.bottom else contentImage.bottom,
-                                            margin = 8.dp
-                                        )
-                                    }
-                            ) {
-                                Text(
-                                    text = content,
-                                    style = MaterialTheme.typography.bodyMedium
-                                )
-                            }
-                        } else {
-                            Text(
-                                text = content,
-                                style = MaterialTheme.typography.bodyMedium,
-                                modifier = Modifier
-                                    .constrainAs(contentText) {
-                                        top.linkTo(
-                                            if (image == null) nameUnit.bottom else contentImage.bottom,
-                                            margin = 8.dp
-                                        )
-                                    }
-                            )
-                        }
-
-                        if (isHeartAble) {
-                            Surface(
-                                onClick = { onHeartClicked(!isHeart) },
-                                color = Color.Transparent,
-                                shape = CircleShape,
-                                modifier = Modifier
-                                    .constrainAs(heart) {
-                                        top.linkTo(contentText.bottom, margin = 8.dp)
-                                        centerHorizontallyTo(parent)
-                                    }
-                            ) {
-                                Row(
-                                    verticalAlignment = Alignment.CenterVertically,
-                                    modifier = Modifier.padding(8.dp)
-                                ) {
-                                    Icon(
-                                        imageVector = if (isHeart) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
-                                        tint = if (isHeart) Color.Red else LocalContentColor.current,
-                                        contentDescription = null // TODO
-                                    )
-                                    Spacer(modifier = Modifier.width(8.dp))
-                                    Text(text = "$heartCount")
-                                }
-                            }
-                        }
-
-                        IconButton(
-                            onClick = { isDropDownMenuExpanded = !isDropDownMenuExpanded },
-                            modifier = Modifier
-                                .constrainAs(dropDownMenuButton) {
-                                    top.linkTo(contentText.bottom, margin = 8.dp)
-                                    end.linkTo(parent.end)
-                                }
-                        ) {
-                            Icon(
-                                imageVector = Icons.Filled.MoreVert,
-                                contentDescription = null // TODO
-                            )
-
-                            DropdownMenu(
-                                expanded = isDropDownMenuExpanded,
-                                onDismissRequest = { isDropDownMenuExpanded = false }
-                            ) {
-                                DropdownMenuItem(
-                                    text = { Text(text = "TODO") }, // TODO
-                                    onClick = {
-                                        /*TODO*/
-                                        isDropDownMenuExpanded = false
-                                    }
-                                )
-                            }
-                        }
-
-                    }
-
-                }
-
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(getCornerSize(shape = MaterialTheme.shapes.medium) + 8.dp)
-                ) {
-
-                    AnimatedVisibility(visible = isCommentShowing) {
-                        Column(
-                            modifier = Modifier.fillMaxWidth()
-                        ) {
-                            commentList.forEach { comment ->
-                                Box(
-                                    modifier = Modifier.fillMaxWidth()
-                                ) {
-                                    ChatUnit(chatData = comment)
-                                }
-                            }
-
-                            Spacer(modifier = Modifier.height(8.dp))
-                        }
-                    }
-
-                    AnimatedContent(
-                        targetState = isCommenting
-                    ) {
-                        if (it) {
-                            Surface(
-                                color = MaterialTheme.colorScheme.primary,
-                                contentColor = MaterialTheme.colorScheme.onPrimary,
-                                shape = CircleShape
-                            ) {
-                                OutlinedTextField(
-                                    value = "asdf",
-                                    onValueChange = { value ->
-
-                                    },
-                                    leadingIcon = {
-                                        IconButton(
-                                            onClick = { isCommenting = false }
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Filled.KeyboardArrowLeft,
-                                                contentDescription = null, // TODO
-                                                tint = MaterialTheme.colorScheme.onPrimary
-                                            )
-                                        }
-                                    },
-                                    trailingIcon = {
-                                        IconButton(
-                                            onClick = { /*TODO*/ }
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Filled.Send,
-                                                contentDescription = null, // TODO
-                                                tint = MaterialTheme.colorScheme.onPrimary
-                                            )
-                                        }
-                                    },
-                                    textStyle = MaterialTheme.typography.bodySmall,
-                                    shape = CircleShape,
-                                    modifier = Modifier
-                                        .padding(4.dp)
-                                        .fillMaxWidth()
-                                        .height(40.dp)
-                                )
-                            }
-                        } else {
-                            Box(
-                                modifier = Modifier.fillMaxWidth()
-                            ) {
-                                Button(
-                                    onClick = { isCommentShowing = !isCommentShowing },
-                                    modifier =
-                                    Modifier
-                                        .align(
-                                            BiasAlignment(
-                                                verticalBias = 0f,
-                                                horizontalBias = animateFloatAsState(targetValue = if (isCommentShowing) -1f else 0f).value
-                                            )
-                                        )
-                                ) {
-                                    Row {
-                                        AnimatedVisibility(visible = !isCommentShowing) {
-                                            Icon(
-                                                imageVector = Icons.Filled.Chat,
-                                                contentDescription = null, // TODO
-                                                modifier = Modifier.padding(end = 8.dp)
-                                            )
-                                        }
-                                        Icon(
-                                            imageVector = if (isCommentShowing) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
-                                            contentDescription = null // TODO
-                                        )
-                                    }
-                                }
-
-                                androidx.compose.animation.AnimatedVisibility(
-                                    visible = isCommentShowing,
-                                    modifier = Modifier.align(Alignment.CenterEnd)
-                                ) {
-                                    Button(
-                                        onClick = { isCommenting = true }
-                                    ) {
-                                        Icon(
-                                            imageVector = Icons.Filled.Chat,
-                                            contentDescription = null // TODO
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
             }
         }
     }
